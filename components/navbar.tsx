bacf68b5f4d45e0f269b10f1dcac205540e63193
hclanka <152237163+chandudagoat@users.noreply.github.com>
2024-01-08 00:01:12 +0530

    fin navbar web

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,14 +4,25 @@ import logo from "@/app/portrait.png";
 const Navbar = () => {
   return (
     <nav className="w-full flex items-center justify-around py-8">
+      <a href="/">
+        <Image
+          src={logo}
+          alt="logo"
+          width={150}
+          className="drop-shadow-lg hover:drop-shadow-xl"
+        />
+      </a>
       <div>
-        <Image src={logo} alt="logo" width={150} />
-      </div>
-      <div>
-        <a href="/support" className="mr-4 text-[#88D498]">
+        <a
+          href="/support"
+          className="mr-4 text-[#88D498] font-bold drop-shadow-lg hover:drop-shadow-xl"
+        >
           Support Us
         </a>
-        <a href="/signup" className="">
+        <a
+          href="/signup"
+          className="px-10 py-5 rounded text-[#F3E9D2] bg-[#1A936F] font-bold drop-shadow-lg hover:drop-shadow-xl"
+        >
           Sign Up
         </a>
       </div>
